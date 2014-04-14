9376db6f3d509c59e14b605539eb5f132a1680b1
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-04-14 20:01:13 +0200

    First commit

---
+++ Dashboard/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Dashboard
+//
+//  Created by Benjamin SENECHAL on 14/04/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Dashboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dashboard
+//
+//  Created by Benjamin SENECHAL on 14/04/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
