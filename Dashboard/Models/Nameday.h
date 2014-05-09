2f88a3e00fab3c894cae9a25a9b70c1fd8b6df4b
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-05-09 18:23:09 +0200

    Add Nameday model

---
+++ Dashboard/Models/Nameday.h
@@ -0,0 +1,17 @@
+//
+//  Nameday.h
+//  Dashboard
+//
+//  Created by Benjamin SENECHAL on 09/05/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Nameday : NSObject
+
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSString *gender;
+@property (nonatomic, strong) NSString *name;
+
+@end
---
+++ Dashboard/Models/Nameday.m
@@ -0,0 +1,13 @@
+//
+//  Nameday.m
+//  Dashboard
+//
+//  Created by Benjamin SENECHAL on 09/05/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import "Nameday.h"
+
+@implementation Nameday
+
+@end
